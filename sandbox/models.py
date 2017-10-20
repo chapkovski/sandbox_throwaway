5d8afa463d11ecc4528c982edeb1cdd1707ed111
chapkovski <chapkovski@gmail.com>
2017-10-20 17:55:31 +0200

    asdf

--- sandbox/models.py
+++ sandbox/models.py
@@ -2,7 +2,7 @@ from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
     Currency as c, currency_range
 )
-
+import json
 
 author = 'Your name here'
 
@@ -11,16 +11,28 @@ Your app description
 """
 
 
+
 class Constants(BaseConstants):
     name_in_url = 'sandbox'
     players_per_group = 3
     num_rounds = 10
+    print(locals()['num_rounds'])
+
+    artifact_urls = "Hello, world!"
+    artifact_const =  [artifact_urls for i in artifact_urls]
+    print(artifact_urls)
+    artifact_const = {"url": artifact_urls[0]}
+    # for i in range(1):
+    #     tempdict = {"url": artifact_urls[i]}
+    #     print(tempdict)
+
+
 
 class Subsession(BaseSubsession):
     ...
 
 class Player(BasePlayer):
-    request_player = models.IntegerField(widget=widgets.RadioSelect(),)
+    request_player = models.CurrencyField(widget=widgets.SliderInput(attrs={'step': '5'}))
 
 
 class Group(BaseGroup):
--- sandbox/views.py
+++ sandbox/views.py
@@ -5,13 +5,14 @@ from .models import Constants
 
 
 class MyPage(Page):
+    timeout_seconds=1000
     form_model = models.Player
     form_fields = ['request_player']
 
     def request_player_choices(self):
         choices = []
         for o in self.player.get_others_in_group():
-            choices.append((o.id_in_group, "Player {}".format(o.id_in_group)))
+            choices.append(o.id_in_group)
         return choices
 
 
