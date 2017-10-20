3179dfd9621f0df6e8e5af67fcd73ae3f01b273e
chapkovski <chapkovski@gmail.com>
2017-10-20 18:25:24 +0200

    jhgfj

--- sandbox/models.py
+++ sandbox/models.py
@@ -16,15 +16,7 @@ class Constants(BaseConstants):
     name_in_url = 'sandbox'
     players_per_group = 3
     num_rounds = 10
-    print(locals()['num_rounds'])
 
-    artifact_urls = "Hello, world!"
-    artifact_const =  [artifact_urls for i in artifact_urls]
-    print(artifact_urls)
-    artifact_const = {"url": artifact_urls[0]}
-    # for i in range(1):
-    #     tempdict = {"url": artifact_urls[i]}
-    #     print(tempdict)
 
 
 
